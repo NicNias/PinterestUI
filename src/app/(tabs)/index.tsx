3170ec1f9621341a6ee904a5db9779a2bebdcf93
NaniasNic <ananiasdias0@gmail.com>
2024-02-24 15:23:02 -0300

    filters created

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,12 +1,17 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, StyleSheet } from "react-native";
+
+import { FILTERS } from "../../utils/filters";
 
 import { theme } from "../../theme";
+import { Filters } from "../../components/Filters";
 
 export default function Home() {
+    const [filter, setFilter] = useState(FILTERS[0]);
+
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>Home</Text>
+            <Filters filters={FILTERS} filter={filter} onChange={setFilter} />
         </View>
     )
 }
@@ -14,9 +19,9 @@ export default function Home() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: theme.colors.black
+        backgroundColor: theme.colors.black,
+        padding: 12,
+        paddingTop: 52,
     },
     text: {
         fontSize: 22,
